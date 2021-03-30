38daec06b455fdca54b6ab007b2ce8c5f7cea6a8
firzatullahd <firzatullahdwikoramadhan@gmail.com>
2021-03-30 15:51:02 +0700

    add address controller

--- models/Address.go
+++ models/Address.go
@@ -11,7 +11,7 @@ type Address struct {
 	CreatedAt time.Time      `json:"created_at" form:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at" form:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at" form:"deleted_at"`
-	UserID    uint           `json:"user_id" form:"user_id"`
+	UserID    uint           `gorm:"foreignKey" json:"user_id" form:"user_id"`
 	Name      string         `gorm:"type:varchar(255);not null" json:"name" form:"name"`
 	Address   string         `gorm:"type:varchar(255);not null" json:"address" form:"address"`
 }
