0226c4c7ad9ce940d0fbdf7fdb26ca2f86b113c6
firzatullahd <firzatullahdwikoramadhan@gmail.com>
2021-03-30 14:18:02 +0700

    add dotenv, implement auth to getShoppingcart controller

--- controllers/shoppingcart.go
+++ controllers/shoppingcart.go
@@ -3,17 +3,18 @@ package controllers
 import (
 	"net/http"
 	"retailStore/config"
+	"retailStore/middlewares"
 	"retailStore/models"
-	"strconv"
 
 	"github.com/labstack/echo"
 )
 
 func GetShoppingCartController(c echo.Context) error {
-	id := c.QueryParam("shopping_cart_id")
-	val, _ := strconv.Atoi(id)
+	id := middlewares.ExtractTokenUserId(c)
+	// id := c.QueryParam("shopping_cart_id")
+	// val, _ := strconv.Atoi(id)
 	model := models.ShoppingCart{
-		ID: uint(val),
+		ID: uint(id),
 	}
 
 	shoppingCart := models.ShoppingCart{}
--- routes/routes.go
+++ routes/routes.go
@@ -13,19 +13,21 @@ func New() *echo.Echo {
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
 	middlewares.LogMiddlewares(e)
-
+	
+	e.POST("/register", controllers.CreateUserController)
+	e.POST("/login", controllers.LoginUserController)
+	
 	e.GET("/users", controllers.GetUsersController)
 	e.GET("/users/:id", controllers.GetUserByIdController)
-
-	e.POST("/users", controllers.CreateUserController)
 	e.DELETE("/users/:id", controllers.DeleteUserByIdController)
 	e.PUT("/users/:id", controllers.UpdateUserByIdController)
 
-	e.GET("/items/:id", controllers.GetItemWIthParamsController)
-	e.GET("/items", controllers.GetItemController)
-	e.POST("/items", controllers.PostItemController)
 
-	e.GET("/shoppingcarts", controllers.GetShoppingCartController)
+	e.GET("/items", controllers.GetItemController)
+	e.GET("/items/:id", controllers.GetItemWIthParamsController)
+	e.POST("/items", controllers.PostItemController) // admin
+
+	e.GET("/shoppingcarts", controllers.GetShoppingCartController) //authenticated user
 	//e.POST("/shoppingcarts", controllers.PostItemToShoppingCartController)
 
 	eJWT := e.Group("") 
